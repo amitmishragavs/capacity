1c12e78a8588875c9bd0c432d372757f05a7c8bd
amit mishra <amitmishra@amits-MacBook-Pro.local>
2022-09-15 11:14:09 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { LayoutComponent } from "./components/Layout/Layout";
 import { SliderComponent } from "./components/Slider/Slider";
 import './app.scss'
+import { GChart } from "./components/GChart/GChart";
 
 function App() {
   return <LayoutComponent >
     <SliderComponent className="app-wrapper" />
+    <section className="g-chart">
+    <GChart />
+    </section>
   </LayoutComponent>;
 }
 
